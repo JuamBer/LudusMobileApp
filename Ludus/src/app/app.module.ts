92329197c636dec0b949d119da3c40f78d5adfd0
Juan Sáez García <jsaez@odec.es>
2022-04-28 12:49:05 +0200

    instalar paquete conexion ionic

--- Ludus/src/app/app.module.ts
+++ Ludus/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { CommonModule } from '@angular/common'
 
 //IONIC
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { Network } from '@ionic-native/network/ngx';
 
 //MODULES
 import { AppRoutingModule } from './app-routing.module';
@@ -75,7 +76,10 @@ import { environment } from '../environments/environment';
     }),
     EffectsModule.forRoot([GamesEffects, ReviewsEffects, GendersEffects, AuthEffects, TypesEffects, ComplexitiesEffects])
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    Network,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
   bootstrap: [AppComponent],
 
 })
